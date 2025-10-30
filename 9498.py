f5a42893eb04400728ce9333875398498f479e30
mac <mac@macui-MacBook-Pro.local>
2025-10-29 23:36:12 -0700

    틀림 진짜 왜 그러는지 모른다

---
+++ 9498.py
@@ -0,0 +1,17 @@
+grade = input()
+grade = int(grade)
+
+if grade in range (90, 100):
+    print("A")
+
+elif grade in range (80, 89):
+    print("B")
+
+elif grade in range (70, 79):
+    print("C")
+
+elif grade in range (60, 69):
+    print("D")
+
+else:
+    print("F")
