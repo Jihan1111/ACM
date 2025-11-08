3412c3f575b31a111be6e74fa19d6b207bb82861
mac <mac@macui-MacBook-Pro.local>
2025-11-07 18:37:19 -0800

    14681문제완료

--- 2753.py
+++ 2753.py
@@ -1,9 +1,19 @@
 a=int(input())
 if a%400 == 0:
     print("1")
-if a%100 == 0:
+elif a%100 == 0:
     print("0")
 elif a%4 == 0:
     print("1")
 else:
     print("0")
+
+#     a=int(input())
+# if a%400 == 0:
+#     print("1")
+# if a%100 == 0:
+#     print("0")
+# elif a%4 == 0:
+#     print("1")
+# else:
+#     print("0")
