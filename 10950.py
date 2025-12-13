e371f8568dc1db0423288f53af26aaae8a2f93db
mac <mac@macui-MBP.lan>
2025-12-12 17:43:14 -0800

    2739 upd

---
+++ 10950.py
@@ -0,0 +1,7 @@
+q=int(row_input())
+for i in range(0,q+1)
+    a=int(row_input())
+    b=int(row_input())
+    c=a+b
+    print(a"+"b"="c)
+
---
+++ 2739.py
@@ -0,0 +1,7 @@
+#a*b=c
+b=1
+a=int(row_input())
+for i in range(1,11):
+    c=a*b
+    print(a"*"b"="c)
+    b=b+1
