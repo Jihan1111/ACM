985df250b857711097c463b0066bf49b290096a3
mac <mac@macui-MacBook-Pro.local>
2025-12-20 10:19:24 -0800

    2480.py

--- 2480.py
+++ 2480.py
@@ -1,13 +1,15 @@
-a,b,c=raw_input().split(" ")
-a=int(a)
-b=int(b)
-c=int(c)
-
+abc=input()
+a=int(abc.split(" ")[0])
+b=int(abc.split(" ")[1])
+c=int(abc.split(" ")[2])
 if a==b==c:
     money=10000+a*1000
-elif a==b or b==c or a==b:
+elif a==b:
     money=1000+a*100
+elif a==c:
+    money=1000+c*100
+elif b==c:
+    money=1000+b*100
 else:
-    money=max(a,b,c)
+    money=100*(max(a,b,c))
 print(money)
-#eS,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
