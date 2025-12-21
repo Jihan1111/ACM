817ff2e3be84de91eed40437d590fe3e3bee971e
mac <mac@macui-mbp.local.ftr>
2025-12-21 12:46:50 -0800

    11022완료

--- 11022.py
+++ 11022.py
@@ -1,10 +1,14 @@
 t=int(input())
 l=[]
+al=[]
+bl=[]
 for i in range(1,t+1):
     ab=input()
     a=int(ab.split(" ")[0])
+    al.append(a)
     b=int(ab.split(" ")[1])
+    bl.append(b)
     c=a+b
     l.append(c)
 for h in range(1,t+1):
-    print("Case ","#",h,": ",l[h-1],sep='')
+    print("Case ","#",h,": ",al[h-1]," + ",bl[h-1]," = ",l[h-1],sep='')
