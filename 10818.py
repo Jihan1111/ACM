0ae07ab08dd63e04577ddc9d61f8b9a090d193c4
mac <mac@macui-MacBook-Pro.local>
2026-01-16 18:51:32 -0800

    10818완료

--- 10818.py
+++ 10818.py
@@ -6,14 +6,6 @@ b=0
 num=None
 for base in range(0,n):
     l.append(int(al.split(" ")[base]))
-#print(l)
 anum=min(l)
 bnum=max(l)
-# for chack in range(0,n):
-#     if l[chack]<l[anum]:
-#         anum=l[chack]
-#     elif l[chack]>l[bnum]:
-#         bnum=l[chack]
-#     else:
-#         pass
 print(anum,bnum)
